a8fdad78e93569c1c2635116c7038c44c4230732
James-2004 <jamessaba1804@gmail.com>
2024-04-10 11:25:42 +0200

    tp4

--- src/produit/Unite.java
+++ src/produit/Unite.java
@@ -1,7 +1,7 @@
 package produit;
 
 public enum Unite {
-	LITRE("l"), GRAMME("G"), KILOGRAMME("K"), CENTILITRE("C"), MILILITRE("M"), PAR_PIECE("P");
+	LITRE("l"), GRAMME("g"), KILOGRAMME("Kg"), CENTILITRE("Cl"), MILILITRE("Ml"), PAR_PIECE("P");
 	private final String unit ;
 	
 	Unite(String string) {
